cb5067becd77a76faaa6d879f4d86c06760ff039
nkhash1 <112453090+nkhash1@users.noreply.github.com>
2022-08-30 14:21:19 +0530

    Update script.py

--- Uploader/script.py
+++ Uploader/script.py
@@ -92,4 +92,4 @@ Bot to remotely upload files to telegram.
     NO_VOID_FORMAT_FOUND = "ERROR... <code>{}</code>"
     FREE_USER_LIMIT_Q_SZE = """Cannot Process Free users only 1 request per 4 hrs\n
 Upgrade your /plans to Remove Time Gaps and For link Processing"""
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    SLOW_URL_DECED = "Gosh that seems to be a very slow URL."
